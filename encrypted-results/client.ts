949fa785a4a03876100a1c73ff9b718d79d652b8
Nathan Stitt <nathan@stitt.org>
2024-12-03 12:32:08 -0600

    extract public key from private; convert to class

--- encrypted-results/client.ts
+++ encrypted-results/client.ts
@@ -2,118 +2,124 @@ import { ZipReader, BlobWriter, BlobReader, TextWriter, type Entry } from '@zip.
 import type { ResultsManifest, ResultsFile } from './types.js';
 
 
-// IN REALITY, this would be stored with the member or researcher's profile
-const publicKeyPEM = `-----BEGIN PUBLIC KEY-----
-MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAs4G6qWTMu9cSiYraFLiE
-HDX1aKVqyLqMl5ulMk9PHpynTPbY8edjxBKyFsbFt10TE2K+GxmwCl6cFfrXtZYR
-kxcJT75pL/w93jm2KPs3rxyCIh5cpd9oXF03TJ32fklppS440SQjP1YPBUzaYGha
-Dobmar7a/jOMZPvY30mfvLHLhFNOUlwlo/ztZ6i9cu/AO7UkwAhkKT/KIdVX/iOi
-KgjnvO2KmW0IVQp1N4yREWFZTiTl3VnCxRTqcE9P49e2pQIIJ/uAMhBWokP5v5bt
-zoZD1dgoyoq8hDGnAovsbXmHE/UkVVAwZu428Y1WTpxumBndugopDglzYWdCR8Nt
-imzfA0RVb7s5kIcFAQ2v7lcu7EW5cn6jyOSpDWaQxTd/w+vYuUtNp2MGP5ecSAHe
-UN5UaZ1WH7W3JoTnvTbd5em7Y4mhC3XhrKFKiXc6XBMZL71NyoiQcuEFZ76QiR2p
-/X8RCbcuqxAzR2hcVFvEqYMZGguOIN/TzXKKvDefG9ajLLYSYwvGMU+2u3ZPgnme
-gbDoM0P9lCJxDzKLBpSW/B/2NqsNsZva7T2NrJDvjCmWOarAmHnEtJdl6TMkdzsM
-+zXXEqdir7jGcbrOg0u2Dve4Rvt1UxxWEWZtXl3+2qUstjzv8Ven06XlJp3IN4Dv
-Z++j6KixG43Cmi2uws8lIM8CAwEAAQ==
------END PUBLIC KEY-----`
+class Results {
+    zip: ZipReader
+    privateKey: CryptoKey
+    publicKey: CryptoKey
+    entries: Entry[]
+    files: Record<string, string> = {}
 
+    async initialize(zipBlob: Blob, pkey: string) {
+        this.zip = new ZipReader(new BlobReader(zipBlob))
+        this.privateKey = await crypto.subtle.importKey(
+            "pkcs8",
+            this.pemToDer(pkey),
+            { name: "RSA-OAEP", hash: { name: "SHA-256" } },
+            true, // must be extractable, we need to export public key later
+            ["decrypt"]
+        );
+        const jwk = await crypto.subtle.exportKey("jwk", this.privateKey);
+        this.publicKey = await crypto.subtle.importKey(
+            "jwk",
+            {
+                kty: jwk.kty,
+                n: jwk.n,
+                e: jwk.e,
+                alg: "RS256", // Algorithm identifier for RSASSA-PKCS1-v1_5 with SHA-256
+                ext: true,
+            },
+            { name: "RSASSA-PKCS1-v1_5", hash: { name: "SHA-256" } },
+            true, // Extractable
+            ["verify"] // Key usages
+        );        
+        this.entries = await this.zip.getEntries();
+        return this
+    }
 
-function decodeBase64(base64String: string) {
-    return Uint8Array.from(atob(base64String), char => char.charCodeAt(0)).buffer;
-}
+    async extractManifest() {
+        for (const entry of this.entries) {
+            if (entry.getData && entry.filename == "manifest.json") {
+                const manifestText = await entry.getData(new TextWriter())
+                const manifest = JSON.parse(manifestText) as ResultsManifest
 
-function pemToDer(pem: string) {
-    const pemContents = pem
-        .replace(/-+BEGIN (PUBLIC|PRIVATE) KEY-+/, '')
-        .replace(/-+END (PUBLIC|PRIVATE) KEY-+/, '')
-        .replace(/\s/g, "");
+                for (const [signature, entry] of Object.entries(manifest.audit)) {
+                    const isValid = await crypto.subtle.verify(
+                        "RSASSA-PKCS1-v1_5",
+                        this.publicKey,
+                        Uint8Array.from(atob(signature), (c) => c.charCodeAt(0)),
+                        new TextEncoder().encode(JSON.stringify(entry))
+                    );
+                    if (!isValid) {
+                        throw new Error(`Invalid signature for entry: ${JSON.stringify(entry, null, 2)}`)
+                    }
+                }
+                
+                
+                this.manifest = manifest
+            }
+        }
+        return this
+    }
 
-    return decodeBase64(pemContents) 
-}
-
-
-async function validateEntry(signature: string, data: string) {
-    const publicKey = await crypto.subtle.importKey(
-        "spki",
-        pemToDer(publicKeyPEM),
-        {
-            name: "RSASSA-PKCS1-v1_5",
-            hash: "SHA-256",
-        },
-        true,
-        ["verify"]
-    );
-
-    return await crypto.subtle.verify(
-        "RSASSA-PKCS1-v1_5",
-        publicKey,
-        Uint8Array.from(atob(signature), (c) => c.charCodeAt(0)),
-        new TextEncoder().encode(data)
-    );
-}
-
-
-async function validateManifestOrThrow(manifest: ResultsManifest) {
-    for (const [signature, entry] of Object.entries(manifest.audit)) {
-        if (!validateEntry(signature, JSON.stringify(entry))) {
-            throw new Error(`Invalid signature for entry: ${JSON.stringify(entry, null,2 )}`)
+    async extractFiles() {
+        for (const entry of this.entries) {
+            // skip directories and manifest because we already processed it
+            if (!entry.getData || entry.filename == 'manifest.json') {
+                continue
+            }
+            const file = this.manifest.files[entry.filename]
+            if (!file) {
+                throw new Error(`No metadata found for file: ${entry.filename}`)
+            }
+            await this.extractFileEntry(entry, file)
+            //            displayFile(entry.filename, contents)
         }
     }
-}
 
+    async extractFileEntry(entry: Entry, file: ResultsFile) {
+        const aesKeyBuffer = await crypto.subtle.decrypt(
+            { name: "RSA-OAEP" },
+            this.privateKey,
+            this.decodeBase64(file.key)
+        );
 
-async function decryptFileEntry(privateKeyPEM, entry: Entry, file: ResultsFile) { 
-    const privateKey = await crypto.subtle.importKey(
-        "pkcs8",
-        pemToDer(privateKeyPEM),
-        {
-            name: "RSA-OAEP",
-            hash: { name: "SHA-256" },
-        },
-        false,
-        ["decrypt"]
-    );
+        const aesKey = await crypto.subtle.importKey(
+            "raw",
+            aesKeyBuffer,
+            { name: "AES-CBC" },
+            false,
+            ["decrypt"]
+        );
 
-    const aesKeyBuffer = await crypto.subtle.decrypt(
-        { name: "RSA-OAEP" },
-        privateKey,
-        decodeBase64(file.key)  
-    );
+        const contentsBlob = await entry.getData(new BlobWriter())
+        const decryptedDataBuffer = await crypto.subtle.decrypt(
+            { name: "AES-CBC", iv: this.decodeBase64(file.iv) },
+            aesKey,
+            await contentsBlob.arrayBuffer(),
+        );
 
-    const aesKey = await crypto.subtle.importKey(
-        "raw",
-        aesKeyBuffer,
-        { name: "AES-CBC" },
-        false,
-        ["decrypt"]
-    );
-
-    const contentsBlob = await entry.getData(new BlobWriter())
-    const decryptedDataBuffer = await crypto.subtle.decrypt(
-        { name: "AES-CBC", iv: decodeBase64(file.iv) },
-        aesKey,
-        await contentsBlob.arrayBuffer(), 
-    );
-
-    // TODO:  check mime and use binary decoder if it's an image or somethig
-    const decoder = new TextDecoder();
-    const contents =  decoder.decode(decryptedDataBuffer);
+        // TODO:  check mime and use binary decoder if it's an image or somethig
+        const decoder = new TextDecoder();
+        const contents = decoder.decode(decryptedDataBuffer);
+        this.files[entry.filename] = contents
+    }
     
-    return contents
+    
+    decodeBase64(base64String: string) {
+        return Uint8Array.from(atob(base64String), char => char.charCodeAt(0)).buffer;
+    }
+
+    pemToDer(pem: string) {
+        const pemContents = pem
+            .replace(/-+BEGIN (PUBLIC|PRIVATE) KEY-+/, '')
+            .replace(/-+END (PUBLIC|PRIVATE) KEY-+/, '')
+            .replace(/\s/g, "");
+
+        return this.decodeBase64(pemContents)
+    }
+
 }
 
-async function extractManifest(entries: Entry[]) {
-    for (const entry of entries) {
-        if (entry.getData && entry.filename == "manifest.json") {
-            const manifestText = await entry.getData(new TextWriter())
-            const manifest = JSON.parse(manifestText) as ResultsManifest
-            validateManifestOrThrow(manifest)
-            displayFile('manifest.json', JSON.stringify(manifest, null, 4))
-            return manifest
-        }
-    }
-}
 
 function displayFile(fileName: string, contents: string) {
     document.getElementById('files-panel')!.innerHTML += `<div>
@@ -130,31 +136,20 @@ document.getElementById('decrypt-btn')!.addEventListener('click', async () => {
     
     fetch('/results').then(async (res) => {
         document.getElementById('files-panel')!.style.display = 'flex';
-        const privateKeyPEM = document.getElementById('privateKeyPEM')!.value
         
-        const zip = new ZipReader(new BlobReader(await res.blob()))
-        const entries = await zip.getEntries();
-        const manifest = await extractManifest(entries)
-        if (!manifest) {
-            throw new Error('Manifest not found')
-        }
+        const privateKeyPEM = document.getElementById('privateKeyPEM')!.value
 
-        for (const entry of entries) {
-            // skip directories and manifest because we already processed it
-            if (!entry.getData || entry.filename == 'manifest.json') {
-                continue
-            }
-
-            const file = await entry.getData(new BlobWriter())
-            const url = URL.createObjectURL(file)
-            const data = manifest.files[entry.filename]
-            if (!data) {
-                throw new Error(`No metadata found for file: ${entry.filename}`)
-            }
-            const contents = await decryptFileEntry(privateKeyPEM, entry, data)
-            displayFile(entry.filename, contents)
+        const results = new Results()
+        await results.initialize(await res.blob(), privateKeyPEM)
+        await results.extractManifest()
+        displayFile('manifest.json', JSON.stringify(results.manifest, null, 4))
+        await results.extractFiles()
+        for (const [fileName, contents] of Object.entries(results.files)) {
+            displayFile(fileName, contents)
         }
         
     })
 
 })
+
+// document.getElementById('decrypt-btn').click()
